5cd80cb1bd90c7ed518bfdd09c3a1b8f0fbe6640
hei4 <hei4.1981@gmail.com>
2018-10-20 13:21:11 +0900

    delete view method in Discriminator

--- part1/chapter6/net.py
+++ part1/chapter6/net.py
@@ -115,5 +115,5 @@ class Discriminator(nn.Module):
         """
         for layer in self.layers.values():  # self.layersの各層で演算を行う
             x = layer(x)
-        return x.view(-1, 1).squeeze(1)     # テンソルの形状を(B, 1)に変更した後に(B,)に変更して戻り値とする
+        return x.squeeze()     # テンソルの形状を(B)に変更して戻り値とする
 
